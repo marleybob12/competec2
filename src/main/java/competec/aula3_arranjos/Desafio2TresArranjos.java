ca972d550f069e06323445609c6c989794bf2223
marleybob12 <167625143+marleybob12@users.noreply.github.com>
2024-05-18 14:40:50 +0000

    a

--- src/main/java/competec/aula3_arranjos/Desafio2TresArranjos.java
+++ src/main/java/competec/aula3_arranjos/Desafio2TresArranjos.java
@@ -3,11 +3,12 @@ package competec.aula3_arranjos;
 public class Desafio2TresArranjos {
     public static void main(String[] args) {
         // Crie um vetor com 3 números inteiros a sua escolha:
-
+        int numeros[] = {3,2,9};
+     
         // Crie uma variável que irá armazenar a soma desses 3 valores:
-
+int soma= numeros[0]+numeros[2]+numeros[1];
         // Imprima o resultado dessa variável:
-
+ System.out.println(soma);
         // Extra
         // [] - Faça uma estrutura de repeticao for ou while que imprima cada um dos valores
     }
--- src/main/java/competec/aula3_arranjos/Desafio3EntradaSaidaArranjos.java
+++ src/main/java/competec/aula3_arranjos/Desafio3EntradaSaidaArranjos.java
@@ -1,9 +1,11 @@
 package competec.aula3_arranjos;
 
+import java.util.Scanner;
+
 public class Desafio3EntradaSaidaArranjos {
 
 	public static void main(String[] args) {
-
+	
 		// Coletar tamanho arranjo
 
 		// Preencher arranjo
@@ -14,6 +16,8 @@ public class Desafio3EntradaSaidaArranjos {
 		 * Desafio extra:
 		 * Crie outro arranjo e inverta a ordem dos valores do primeiro arranjo.
 		 */
+
+
 
 	}
 
